921b0a9941283a77852d73709f9589f2748c7aa0
sbpark88 <saebyulpark88@gmail.com>
2022-09-08 08:05:42 +0900

    Update: Complete the section 2 - 'Initializing an Empty string'
    2. Initializing an Empty string
    
    Ref: https://docs.swift.org/swift-book/LanguageGuide/StringsAndCharacters.html

--- Chapter 2 - Strings and Characters.playground/Pages/2. Initializing an Empty string.xcplaygroundpage/Contents.swift
+++ Chapter 2 - Strings and Characters.playground/Pages/2. Initializing an Empty string.xcplaygroundpage/Contents.swift
@@ -1,9 +1,18 @@
 import Foundation
 
 // 2. Initializing an Empty String (빈 문자열 초기화)
+// Both initializing are equal.
+var emptyString = ""
+var initializerSyntax = String()
 
 
 // String Mutability (문자열 수정)
+// If you assign the string to a variable, it can be modified.
+var variableString = "Horse"
+variableString += " and carriage"
+print(variableString)
 
-
-
+// While, if you assign it to a constant, it cannot be modified.
+let constantString = "Highlander"
+constantString += " and another Highlander" // error occured!
+print(constantString)
