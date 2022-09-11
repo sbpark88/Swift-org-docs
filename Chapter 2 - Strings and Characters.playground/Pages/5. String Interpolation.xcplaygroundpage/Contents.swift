f7a7db16ed372d1aa0f24ae2b1f64042b725b4c9
sbpark88 <saebyulpark88@gmail.com>
2022-09-11 18:35:38 +0900

    Update: Complete the section 5 - 'String Interpolation'
    5. String Interpolation
    
    Ref: https://docs.swift.org/swift-book/LanguageGuide/StringsAndCharacters.html

--- Chapter 2 - Strings and Characters.playground/Pages/5. String Interpolation.xcplaygroundpage/Contents.swift
+++ Chapter 2 - Strings and Characters.playground/Pages/5. String Interpolation.xcplaygroundpage/Contents.swift
@@ -1,3 +1,28 @@
 import Foundation
 
 // 5. String Interpolation (문자열 삽입)
+let subject = """
+String interpolation is a way to construct a new String value
+from a mix of constants, variables, literals, and expressions
+by including their values inside a string literal.
+
+"""
+print(subject)
+
+let three = 3
+print("\(three) times 2.5 is \(Double(three) * 2.5)")
+
+print("\nAlso you can string interpolation in the 'multiline string'.")
+let multilineString = """
+
+     \(three)
+x    2.5
+---------
+     \(Double(three) * 2.5)
+
+"""
+print(multilineString)
+
+print("The 'string interpolation' in the 'extended string delimiters'.")
+print(#"\(three) times 2.5 is \(Double(three) * 2.5)"#)     // Ignored
+print(#"\#(three) times 2.5 is \#(Double(three) * 2.5)"#)   // Works well
