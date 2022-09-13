4fde443b48720aefe21ca5cf33baf64706fe45fd
sbpark88 <saebyulpark88@gmail.com>
2022-09-13 23:54:05 +0900

    Update: Complete the section 7 - 'Counting Characters'
    7. Counting Characters
    
    Ref: https://docs.swift.org/swift-book/LanguageGuide/StringsAndCharacters.html

--- Chapter 2 - Strings and Characters.playground/Pages/7. Counting Characters.xcplaygroundpage/Contents.swift
+++ Chapter 2 - Strings and Characters.playground/Pages/7. Counting Characters.xcplaygroundpage/Contents.swift
@@ -1,3 +1,16 @@
 import Foundation
 
 // 7. Counting Characters (문자 세기)
+let unusualMenagerie = "Koala 🐨, Snail 🐌, Penguin 🐧, Dromedary 🐪"
+print("unusualMenagerie has \(unusualMenagerie.count) characters")
+
+var word = "cafe"
+print("the number of character in \(word) is \(word.count)")  // 4
+
+word += "\u{301}" // Combining acute accent, U_0301
+print("\nthe number of character in \(word) is \(word.count)")  // Still, 4
+let message = """
+The extended grapheme clusters may not always affect a string's character count.
+However, this do not means same amount of memeory store.
+"""
+print(">> The extended grapheme clusters may not always affect a string's character count.")
