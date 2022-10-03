ea10ae1cdd4cd3a6431bf2ca1abf75e2d5674530
sbpark88 <saebyulpark88@gmail.com>
2022-10-03 23:33:14 +0900

    Add: 'Chapter 3 Collection Types'
    Complete the section 1 - Mutability of Collections
    
    Ref: https://docs.swift.org/swift-book/LanguageGuide/CollectionTypes.html

---
+++ Chapter 3 - Collection Types.playground/Pages/1. Mutability of Collections.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+// 1. Mutability of Collections (콜렉션의 변경)
+
+/*
+Swift provides three primary collection types for storing collections of values.
+- Array
+- Set
+- Dictionary
+
+These are always clear about the types of values and keys that they can store,
+so you cannot insert a value of the wrong type into a collection by mistake.
+
+If you create an array, a set, or a dictionary, and assign it to a variable(var), the collections that's will be mutable.
+However, if you assign it to a constant(let), the collections is immutable, and its size and contents cannot be changed.
+
+*/
