e53c8cc32b7e32d485f3ab54521e4026c0a1e43b
sbpark88 <saebyulpark88@gmail.com>
2022-10-19 23:55:32 +0900

    Update: Complete the section 1 - Defining and Calling Functions
    Ref: https://docs.swift.org/swift-book/LanguageGuide/Functions.html

--- Chapger 5 - Functions.playground/Pages/1. Defining and Calling Functions.xcplaygroundpage/Contents.swift
+++ Chapger 5 - Functions.playground/Pages/1. Defining and Calling Functions.xcplaygroundpage/Contents.swift
@@ -1,4 +1,11 @@
 import Foundation
 
-var greeting = "Hello, playground"
+// 1. Defining Functions
+func greet(person: String) -> String {
+    "Hello, \(person)!"
+}
 
+
+// 2. Calling Functions
+
+print(greet(person: "Anna"))
