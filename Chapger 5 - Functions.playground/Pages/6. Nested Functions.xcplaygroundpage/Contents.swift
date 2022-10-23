f1afb42fd0319cc094632cecdcd5c22351ee612a
sbpark88 <saebyulpark88@gmail.com>
2022-10-23 23:33:46 +0900

    Update: Complete the section 6 - Nested Functions
    Ref: https://docs.swift.org/swift-book/LanguageGuide/Functions.html

--- Chapger 5 - Functions.playground/Pages/6. Nested Functions.xcplaygroundpage/Contents.swift
+++ Chapger 5 - Functions.playground/Pages/6. Nested Functions.xcplaygroundpage/Contents.swift
@@ -1,3 +1,31 @@
 import Foundation
 
-var greeting = "Hello, playground"
+
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    func stepForward(_ input: Int) -> Int {
+        print(#function)
+        return input + 1
+    }
+    func stepBackward(_ input: Int) -> Int {
+        print(#function)
+        return input - 1
+    }
+    
+    return backward ? stepBackward(_:) : stepForward(_:)
+}
+
+
+func movingStart(initialValue: Int) {
+    var currentValue = initialValue
+    let moveNearToZero = chooseStepFunction(backward: currentValue > 0)
+    
+    print("Conting to zero:")
+    while currentValue != 0 {
+        print("\(currentValue)... Call ", terminator: "")
+        currentValue = moveNearToZero(currentValue)
+    }
+    print("zero!\n")
+}
+
+movingStart(initialValue: 4)
+movingStart(initialValue: -3)
