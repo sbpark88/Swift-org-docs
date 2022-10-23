39f5fb0ecbf81d4d5ae1499a78af72b397a26328
sbpark88 <saebyulpark88@gmail.com>
2022-10-23 13:54:57 +0900

    Update: Complete the section 5 - Function Types
    Ref: https://docs.swift.org/swift-book/LanguageGuide/Functions.html

--- Chapger 5 - Functions.playground/Pages/5. Function Types.xcplaygroundpage/Contents.swift
+++ Chapger 5 - Functions.playground/Pages/5. Function Types.xcplaygroundpage/Contents.swift
@@ -1,3 +1,74 @@
 import Foundation
 
-var greeting = "Hello, playground"
+// 1. Using Function Types
+
+//func addTwoInts(_ a: Int, _ b: Int) -> Int {
+//    a + b
+//}
+//func multiplyTwoInts(_ a: Int, _ b: Int) -> Int {
+//    return a * b
+//}
+//
+//var mathFunction: (Int, Int) -> Int = addTwoInts(_:_:)
+//print(mathFunction(5, 7))   // 12
+//
+//mathFunction = multiplyTwoInts(_:_:)
+//print(mathFunction(5, 7))   // 35
+
+
+let addTwoInts: (Int, Int) -> Int = { (_ a: Int, _ b: Int) in
+    a + b
+}
+
+let multiplyTwoInts = { (_ a: Int, _ b: Int) in
+    a * b
+}
+
+print(addTwoInts(5, 7))         // 12
+print(multiplyTwoInts(5, 7))    // 35
+
+
+print("---------------------------------------")
+// 2. Function Types as Parameter Types
+
+func printMathResult(mathFunction function: (Int, Int) -> Int, _ a: Int, _ b: Int) {
+    print("Result: \(function(a, b))")
+}
+
+printMathResult(mathFunction: addTwoInts, 5, 7)       // Result: 12
+printMathResult(mathFunction: multiplyTwoInts, 5, 7)  // Result: 35
+
+
+print("---------------------------------------")
+// 3. Function Types are Return Types
+
+func stepForward(_ input: Int) -> Int {
+    print(#function)
+    return input + 1
+}
+func stepBackward(_ input: Int) -> Int {
+    print(#function)
+    return input - 1
+}
+
+func chooseStepFunction(backword: Bool) -> (Int) -> Int {
+    backword ? stepBackward(_:) : stepForward(_:)
+}
+
+
+func movingStart(initialValue: Int) {
+    var currentValue = initialValue
+    let moveNearToZero = chooseStepFunction(backword: currentValue > 0)
+
+    print("Conting to zero:")
+    while currentValue != 0 {
+        print("\(currentValue)... Call ", terminator: "")
+        currentValue = moveNearToZero(currentValue)
+    }
+    print("zero!\n")
+}
+
+movingStart(initialValue: 4)
+movingStart(initialValue: -3)
+
+
