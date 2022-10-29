856b45f5b9b4177fe8e4a660f839f224e1e62209
sbpark88 <saebyulpark88@gmail.com>
2022-10-29 23:42:16 +0900

    Update: Complete the section 6 - Autoclosures
    Ref: https://docs.swift.org/swift-book/LanguageGuide/Closures.html

---
+++ Chapter 6 - Closures.playground/Pages/6. Autoclosures.xcplaygroundpage/Contents.swift
@@ -0,0 +1,72 @@
+import Foundation
+
+// 1. Delay evaluation
+//var customersInLine = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+//
+//let customerProvider = { customersInLine.remove(at: 0) }
+//print(customersInLine)          // ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+//print(customersInLine.count)    // 5
+//
+//customerProvider()
+//print(customersInLine)          // ["Alex", "Ewa", "Barry", "Daniella"]
+//print(customersInLine.count)    // 4
+//
+//customerProvider()
+//print(customersInLine)          // ["Ewa", "Barry", "Daniella"]
+//print(customersInLine.count)    // 3
+
+
+// 2.1 Parameter Type is a Closure
+//var customersInLine = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+//
+//func serve(customer customerProvider: () -> String) {
+//    print("Now serving \(customerProvider())!")
+//}
+//
+//serve(customer: { customersInLine.remove(at: 0) })  // Now serving Chris!
+
+
+// 2.2 Parameter Type is an Autoclosure
+var customersInLine = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+func serve(customer customerProvider: @autoclosure () -> String) {
+    print("Now serving \(customerProvider())!")
+}
+
+// `{ }` can be omitted
+serve(customer: customersInLine.remove(at: 0))      // Now serving Chris!
+
+
+// 2.3 Parameter Type is a String
+//var customersInLine = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+//
+//func serve(customer customerProvider: String) {
+//    print("Now serving \(customerProvider)!")
+//}
+//
+//serve(customer: customersInLine.remove(at: 0))      // Now serving Chris!
+
+
+print("-------------------------------------------")
+// 3. Autoclosures with Escaping Closures
+customersInLine = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+//var customersInLine = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+var customerProviders: [() -> String] = []
+func collectCustomerProviders(_ customerProvider: @autoclosure @escaping () -> String) {
+    customerProviders.append(customerProvider)
+}
+collectCustomerProviders(customersInLine.remove(at: 0))
+collectCustomerProviders(customersInLine.remove(at: 0))
+
+print("Collected \(customerProviders.count) closures.")
+
+print("customerProviders: \(customerProviders)")
+
+for customerProvider in customerProviders {
+    print("Now serving \(customerProvider())!")
+}
+
+//Collected 2 closures.
+//[(Function), (Function)]
+//Now serving Chris!
+//Now serving Alex!
