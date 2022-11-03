977ee73cb752a089704ec40643e4c130adba6e39
sbpark88 <saebyulpark88@gmail.com>
2022-11-03 15:35:47 +0900

    Update: Complete the section 3 - Iterating over Enumeration Cases
    Ref: https://docs.swift.org/swift-book/LanguageGuide/Enumerations.html

--- Chapter 7 - Enumerations.playground/Pages/3. Iterating over Enumeration Cases.xcplaygroundpage/Contents.swift
+++ Chapter 7 - Enumerations.playground/Pages/3. Iterating over Enumeration Cases.xcplaygroundpage/Contents.swift
@@ -1,1 +1,12 @@
 import Foundation
+
+enum Beverage: CaseIterable {
+    case coffee, tea, juice
+}
+let numberOfChoices = Beverage.allCases.count
+
+print("\(numberOfChoices) beverages available\n")
+
+for beverage in Beverage.allCases {
+    print(beverage)
+}
