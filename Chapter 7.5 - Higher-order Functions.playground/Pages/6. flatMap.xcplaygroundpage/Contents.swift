ffb46d6360b1343596f5b3d39e33dfc159036af4
sbpark88 <saebyulpark88@gmail.com>
2022-11-16 02:17:24 +0900

    Update: Complete the section 3 - flatMap

--- Chapter 7.5 - Higher-order Functions.playground/Pages/6. flatMap.xcplaygroundpage/Contents.swift
+++ Chapter 7.5 - Higher-order Functions.playground/Pages/6. flatMap.xcplaygroundpage/Contents.swift
@@ -1,4 +1,64 @@
 import Foundation
 
-let numbersWithNil: [Int?] = [5, 15, nil, 3, 9, 12, nil, nil, 17, nil]
+let marks = [[3, 4, 5], [2, 5, 3], [1, 2, 2], [5, 5, 4], [3, 5, 3]]
 
+// 1. 2D Array > 1D Array
+//var allMarks: [Int] = []
+//for marksArray in marks {
+//    allMarks += marksArray
+//}
+
+let allMarks = marks.flatMap { $0 }
+
+print(allMarks) // [3, 4, 5, 2, 5, 3, 1, 2, 2, 5, 5, 4, 3, 5, 3]
+
+
+// 2. another usage
+//var sum: Int = 0
+//for marksArray in marks {
+//    for element in marksArray {
+//        sum += element
+//    }
+//}
+
+let sum = marks
+    .flatMap { $0 }
+    .reduce(0) { $0 + $1 }
+
+print(sum)  // 52
+
+
+// 3. Composite case
+let marksWithNil = [[3, nil, 5], [2, 5, nil], [1, 2, 2], [5, 5, 4], [nil, 5, 3]]
+
+//var anotherSum: Int = 0
+//for marksArray in marksWithNil {
+//    for element in marksArray where element != nil {
+//        anotherSum += element!
+//    }
+//}
+
+//for marksArray in marksWithNil {
+//    for element in marksArray {
+//        guard let element = element else { continue }
+//        anotherSum += element
+//    }
+//}
+
+//for marksArray in marksWithNil {
+//    for element in marksArray {
+//        anotherSum += element ?? 0
+//    }
+//}
+
+//let anotherSum = marksWithNil
+//    .flatMap { $0 }
+//    .filter { $0 != nil }
+//    .reduce(0) { $0 + $1! }
+
+let anotherSum = marksWithNil
+    .flatMap { $0 }
+    .reduce(0) { $0 + ($1 ?? 0) }
+
+
+print(anotherSum)   // 42
