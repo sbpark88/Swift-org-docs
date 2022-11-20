d8e71cd95c14a1204011557b9ffb3d4d9bce967e
sbpark88 <saebyulpark88@gmail.com>
2022-11-20 22:56:17 +0900

    Update: Complete the section 3 - split

--- Chapter 7.5 - Higher-order Functions.playground/Pages/12. split.xcplaygroundpage/Contents.swift
+++ Chapter 7.5 - Higher-order Functions.playground/Pages/12. split.xcplaygroundpage/Contents.swift
@@ -1,1 +1,26 @@
 import Foundation
+
+// 1. split(separator:maxSplits:omittingEmptySubsequences:)
+//let line = "BLANCHE:   I don't want realism. I want magic!"
+//let splited = line.split(separator: " ")
+//print(splited)          // ["BLANCHE:", "I", "don\'t", "want", "realism.", "I", "want", "magic!"]
+//print(type(of: line))       // String
+//print(type(of: splited))    // Array<Substring>
+//
+//
+//print("-----------------------------------------")
+//let splitedMaxOne = line.split(separator: " ", maxSplits: 1)
+//print(splitedMaxOne)    // ["BLANCHE:", "  I don\'t want realism. I want magic!"]
+//
+//
+//print("-----------------------------------------")
+//let splitedOmitFalse = line.split(separator: " ", omittingEmptySubsequences: false) // default true
+//print(splitedOmitFalse) // ["BLANCHE:", "", "", "I", "don\'t", "want", "realism.", "I", "want", "magic!"]
+
+
+print("-----------------------------------------")
+// 2. split(maxSplits:omittingEmptySubsequences:whereSeparator:)
+let line = "BLANCHE:   I don't want realism. I want magic!"
+let splited = line.split { $0 == " " }
+print(splited)          // ["BLANCHE:", "I", "don\'t", "want", "realism.", "I", "want", "magic!"]
+print(type(of: splited))    // Array<Substring>
