1c2bb82d566efe656b0ab1e05008c6ecbc238d16
sbpark88 <saebyulpark88@gmail.com>
2022-11-27 19:06:40 +0900

    Update: Complete the section 6 - Type Properties
    Ref: https://docs.swift.org/swift-book/LanguageGuide/Properties.html

--- Chapter 9 - Properties.playground/Pages/6. Type Properties.xcplaygroundpage/Contents.swift
+++ Chapter 9 - Properties.playground/Pages/6. Type Properties.xcplaygroundpage/Contents.swift
@@ -1,1 +1,85 @@
 import Foundation
+
+// 1. Syntax
+struct SomeStructure {
+    static var someTypeProperty = "Initiate Value"
+    static var computedTypeProoperty: Int {
+        return 1
+    }
+}
+
+enum SomeEnumeration {
+    static var someTypeProperty = "Initiate Value"
+    static var computedTypeProoperty: Int {
+        return 6
+    }
+}
+
+class SomeClass {
+    static var someTypeProperty = "Initiate Value"
+    static var computedTypeProperty: Int {
+        return 27
+    }
+    class var overrideableComputedTypeProperty: Int {
+        return 107
+    }
+}
+
+// 2. Difference between `Stored Type Properties` and `Stored Properties`
+struct AnotherStructure {
+    static var storedTypeProperty = "Apple"
+    var storedProperty = "Pear"
+    
+    static var computedTypeProperty: Int { 1 }
+    var computedProperty: Int { 10 }
+}
+
+// Type Properties
+print(AnotherStructure.storedTypeProperty)   // Apple
+print(AnotherStructure.computedTypeProperty) // 1
+
+AnotherStructure.storedTypeProperty = "Melon"
+print(AnotherStructure.storedTypeProperty)   // Melon
+
+
+// Instance Properties
+var anotherStructure = AnotherStructure()
+print(anotherStructure.storedProperty)       // Pear
+print(anotherStructure.computedProperty)     // 10
+
+anotherStructure.storedProperty = "Watermelon"
+print(anotherStructure.storedProperty)       // Watermelon
+
+var theOtherStructure = AnotherStructure()
+print(theOtherStructure.storedProperty)      // Pear
+
+print(AnotherStructure.storedTypeProperty)   // Melon
+
+
+print("--------------------------------------")
+// 2. AudioChannel Examples
+struct AudioChannel {
+    static let thresholdLevel = 10
+    static var maxInputLevelForAllChannels = 0
+    var currentLevel: Int = 0 {
+        didSet {
+            if currentLevel > AudioChannel.thresholdLevel {
+                currentLevel = AudioChannel.thresholdLevel
+            }
+            if currentLevel > AudioChannel.maxInputLevelForAllChannels {
+                AudioChannel.maxInputLevelForAllChannels = currentLevel
+            }
+        }
+    }
+}
+
+var leftChannel = AudioChannel()
+var rightChannel = AudioChannel()
+
+leftChannel.currentLevel = 7
+print(leftChannel.currentLevel)     // 7
+print(AudioChannel.maxInputLevelForAllChannels) // 7
+
+rightChannel.currentLevel = 11
+print(rightChannel.currentLevel)    // 10
+print(AudioChannel.maxInputLevelForAllChannels) // 10
