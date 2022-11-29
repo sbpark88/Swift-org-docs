c55bf66ab81c18390e09cd28c1005aa9a0e484c5
sbpark88 <saebyulpark88@gmail.com>
2022-11-29 13:31:42 +0900

    Update: Complete the section 1 - Interitance
    Ref: https://docs.swift.org/swift-book/LanguageGuide/Inheritance.html

--- Chapter 12 - Inheritance.playground/Pages/1. Base Classes.xcplaygroundpage/Contents.swift
+++ Chapter 12 - Inheritance.playground/Pages/1. Base Classes.xcplaygroundpage/Contents.swift
@@ -1,1 +1,11 @@
 import Foundation
+
+class Vehicle {
+    var currentSpeed = 0.0
+    var description: String {
+        return "traveling at \(currentSpeed) miles per hour"
+    }
+    func makeNoise() {
+        // do nothing - an arbitrary vehicle doesn't necessarily make a noise
+    }
+}
