9eec7fb9faab56ea3e402a6c7ed3499d307b6cac
sbpark88 <saebyulpark88@gmail.com>
2022-11-30 07:11:00 +0900

    Update: Complete the section 4 - Overriding Stored Properties
    Ref: https://docs.swift.org/swift-book/LanguageGuide/Inheritance.html

--- Chapter 12 - Inheritance.playground/Pages/4. Overriding Stored Properties.xcplaygroundpage/Contents.swift
+++ Chapter 12 - Inheritance.playground/Pages/4. Overriding Stored Properties.xcplaygroundpage/Contents.swift
@@ -1,73 +1,95 @@
 import Foundation
 
-// 1. Overriding
-class Vehicle {
-    var tag = "Vehicle"
-    var currentSpeed = 0.0
-    var description: String {
-        return "traveling at \(currentSpeed) miles per hour"
-    }
-    func makeNoise() {
-        // do nothing - an arbitrary vehicle doesn't necessarily make a noise
-    }
-    init() {
-        tag = "Vehicle2"
-    }
-}
-
-class Bicycle: Vehicle {
-//    private var _tag = "Bicycle"
-//    override var tag: String {
-//        get { _tag }
-//        set { _tag = newValue }
-//    }
-    var hasBasket = false
-    
-    func getSuperTag() -> String {
-        super.tag
-    }
-    
-    override init() {
-        tag = "Bicycle"
-    }
-}
-
-//class Tandem: Bicycle {
-//    private var _tag = "Tandem"
-//    override var tag: String {
-//        get { _tag }
-//        set { _tag = newValue }
-//    }
-//    var currentNumberOfPassengers = 0
+// 1. Origin
+//class Vehicle {
+//    var tag = "Vehicle"
+//}
 //
-//    override func getSuperTag() -> String {
-//        super.tag
+//class Bicycle: Vehicle {
+//}
+//
+//class Tandem: Bicycle {
+//}
+//
+//var vehicle = Vehicle()
+//var bicycle = Bicycle()
+//var tandem = Tandem()
+//print(vehicle.tag)  // Vehicle
+//print(bicycle.tag)  // Vehicle
+//print(tandem.tag)   // Vehicle
+
+
+print("-----------------------------------")
+// 2. Overriding stored properties - Fail
+//class Vehicle {
+//    var tag = "Vehicle"
+//}
+//
+//class Bicycle: Vehicle {
+//    override var tag: String = "Bicycle"    // Cannot override with a stored property 'tag'
+//}
+
+
+print("-----------------------------------")
+// 3. Set property value after initialization - Success
+//class Vehicle {
+//    var tag = "Vehicle"
+//}
+//
+//class Bicycle: Vehicle {
+//}
+//
+//class Tandem: Bicycle {
+//}
+//
+//var vehicle = Vehicle()
+//var bicycle = Bicycle()
+//bicycle.tag = "Bicycle"
+//var tandem = Tandem()
+//tandem.tag = "Tandem"
+//print(vehicle.tag)  // Vehicle
+//print(bicycle.tag)  // Bicycle
+//print(tandem.tag)   // Tandem
+
+
+print("-----------------------------------")
+// 4. Set property values at initialization - Fail
+//class Vehicle {
+//    var tag = "Vehicle"
+//}
+//
+//class Bicycle: Vehicle {
+//    override init() {
+//        tag = "Bicicle" // 'self' used in property access 'tag' before 'super.init' call
 //    }
 //}
 
+
+print("-----------------------------------")
+// 5. Implement computed properties with private stored properties - Success
+class Vehicle {
+    var tag = "Vehicle"
+}
+
+class Bicycle: Vehicle {
+    private var _tag = "Bicycle"
+    override var tag: String {
+        get { _tag }
+        set { _tag = newValue }
+    }
+}
+
+class Tandem: Bicycle {
+    private var _tag = "Tandem"
+    override var tag: String {
+        get { _tag }
+        set { _tag = newValue }
+    }
+}
+
 var vehicle = Vehicle()
-print("vehicle's class name is \(vehicle.tag).")
-
 var bicycle = Bicycle()
-print("bicycle's class name is \(bicycle.tag).")
-//bicycle.tag = "new Bicycle"
-
-//print("vehicle's class name is now \(vehicle.tag).")
-//print("bicycle's class name is now \(bicycle.tag).")
-//print("bicycle's superclass name is now \(bicycle.getSuperTag()).")
-
-//var tandem = Tandem()
-//print("tandem's class name is \(tandem.tag).")
-//print("tandem's superclass name is now \(tandem.getSuperTag()).")
-
-// 2. Accessing Superclass Methods, Properties, and Subscripts
-
-// 3. Overriding Methods
-
-// 4. Overriding Properties
-
-// 5. Overriding Property Getters and Setters
-
-// 6. Overriding Property Observers
-
-
+var tandem = Tandem()
+print(vehicle.tag)  // Vehicle
+print(bicycle.tag)  // Bicycle
+print(tandem.tag)   // Tandem
