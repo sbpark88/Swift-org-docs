937ba2cf83b1d19dc8d1af34d8603b69c488f9e6
sbpark88 <saebyulpark88@gmail.com>
2022-12-05 09:40:43 +0900

    Update: Update the section 5 - Class Inheritance and Initialization
    - Designated and Convenience Initializers in Action
    
    Ref: https://docs.swift.org/swift-book/LanguageGuide/Initialization.html

--- Chapter 13 - Initialization.playground/Pages/5. Class Inheritance and Initialization Part 2.xcplaygroundpage/Contents.swift
+++ Chapter 13 - Initialization.playground/Pages/5. Class Inheritance and Initialization Part 2.xcplaygroundpage/Contents.swift
@@ -1,4 +1,80 @@
 import Foundation
 
 // 7. Designated and Convenience Initializers in Action
+// Base Class
+class Food {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    convenience init() {
+        self.init(name: "[Unnamed]")
+    }
+}
 
+let namedMeat = Food(name: "Bacon")
+print(namedMeat.name)   // Bacon
+
+let mysteryMeat = Food()
+print(mysteryMeat.name) // [Unnamed]
+
+
+print("----------------------------------------------")
+// Subclass
+// Two Designated Initializers
+//class RecipeIngredient: Food {
+//    var quantity: Int
+//    init(name: String, quantity: Int) {
+//        self.quantity = quantity
+//        super.init(name: name)
+//    }
+//    override init(name: String) {
+//        quantity = 1
+//        super.init(name: name)
+//    }
+//}
+
+// One Designated Initializers & One Convenience Initializers override Superclass's Designated Initializers
+class RecipeIngredient: Food {
+    var quantity: Int
+    init(name: String, quantity: Int) {
+        self.quantity = quantity
+        super.init(name: name)
+    }
+    override convenience init(name: String) {
+        self.init(name: name, quantity: 1)
+    }
+}
+
+
+let oneMysteryItem = RecipeIngredient()
+let oneBacon = RecipeIngredient(name: "Bacon")
+let sixEggs = RecipeIngredient(name: "Eggs", quantity: 6)
+
+print("\(oneMysteryItem.name) : \(oneMysteryItem.quantity) ea")
+print("\(oneBacon.name) : \(oneBacon.quantity) ea")
+print("\(sixEggs.name) : \(sixEggs.quantity) ea")
+
+
+print("----------------------------------------------")
+// Subclass of Subclass
+class ShoppingListItem: RecipeIngredient {
+    var purchased = false
+    var description: String {
+        var output = "\(quantity) x \(name)"
+        output += purchased ? " ✔" : " ✘"
+        return output
+    }
+}
+
+var breakfastList = [
+    ShoppingListItem(),
+    ShoppingListItem(name: "Bacon"),
+    ShoppingListItem(name: "Eggs", quantity: 6)
+]
+
+breakfastList[0].name = "Orange juice"
+breakfastList[0].purchased = true
+
+breakfastList.forEach { print($0.description) }
