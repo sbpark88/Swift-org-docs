73b9cad022f22662a8ff21227690544509aecb6b
sbpark88 <saebyulpark88@gmail.com>
2022-12-21 00:02:20 +0900

    Update: Complete the section 1 - Alternative to Forced Unwrapping
    Ref: https://docs.swift.org/swift-book/LanguageGuide/OptionalChaining.html

--- Chapter 15 - Optional Chaining.playground/Pages/1. Alternative to Forced Unwrapping.xcplaygroundpage/Contents.swift
+++ Chapter 15 - Optional Chaining.playground/Pages/1. Alternative to Forced Unwrapping.xcplaygroundpage/Contents.swift
@@ -1,1 +1,42 @@
 import Foundation
+
+class Person {
+    var residence: Residence?
+}
+
+class Residence {
+    var numberOfRooms = 1
+}
+
+let john = Person()
+
+// Forced Unwrapping
+//print(john.residence!.numberOfRooms)
+// Fatal error: Unexpectedly found nil while unwrapping an Optional value
+
+// Optional Binding without Optional Chaining
+if let residence = john.residence {
+    print("John's residence has \(residence.numberOfRooms) room(s).")
+} else {
+    print("john.residence? is nil")
+}
+
+// Optional Binding with Optional Chaining
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s).")
+} else {
+    print("john.residence? is nil")
+}
+
+
+// If the optional has a value?
+john.residence = Residence()
+
+print(john.residence?.numberOfRooms as Any)    // Optional(1)
+
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) room(s).")
+} else {
+    print("john.residence? is nil")
+}
+// John's residence has 1 room(s).
