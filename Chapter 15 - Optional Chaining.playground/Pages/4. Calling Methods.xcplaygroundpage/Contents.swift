edf45b80270743c2a7925806b54ebb75148bfbdc
sbpark88 <saebyulpark88@gmail.com>
2022-12-21 21:12:46 +0900

    Update: Complete the section 4 - Calling Methods Through Optional Chaining
    Ref: https://docs.swift.org/swift-book/LanguageGuide/OptionalChaining.html

--- Chapter 15 - Optional Chaining.playground/Pages/4. Calling Methods.xcplaygroundpage/Contents.swift
+++ Chapter 15 - Optional Chaining.playground/Pages/4. Calling Methods.xcplaygroundpage/Contents.swift
@@ -1,1 +1,77 @@
 import Foundation
+
+let john = Person()
+if john.residence?.printNumberOfRooms() != nil {
+    print("It was possible to print the number of rooms.")
+} else {
+    print("It was not possible to print the number of rooms.")
+}
+// It was not possible to print the number of rooms.
+
+print("----------------------------------------")
+john.residence = {
+    let someResidence = Residence()
+    someResidence.rooms = Array(repeating: "", count: 300).lazy
+        .enumerated().map { (index, value) in
+            index == 237 ? "Shining" : String(index)
+        }
+        .map { Room(name: $0) }
+    someResidence.address = {
+        let someAddress = Address()
+        someAddress.buildingNumber = "29"
+        someAddress.street = "Acacia Road"
+        
+        return someAddress
+    }()
+    
+    return someResidence
+}()
+
+if john.residence?.printNumberOfRooms() != nil {
+    print("It was possible to print the number of rooms.")
+} else {
+    print("It was not possible to print the number of rooms.")
+}
+// The number of rooms is 300
+// It was possible to print the number of rooms.
+
+
+
+/*  Defining Model Classes for Optional Chaining Examples  */
+
+class Person {
+    var residence: Residence?
+}
+
+class Residence {
+    var rooms: [Room] = []
+    var numberOfRooms: Int { rooms.count }
+    subscript(i: Int) -> Room {
+        get { rooms[i] }
+        set { rooms[i] = newValue }
+    }
+    func printNumberOfRooms() {
+        print("The number of rooms is \(numberOfRooms)")
+    }
+    var address: Address?
+}
+
+class Room {
+    let name: String
+    init(name: String) { self.name = name }
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    func buildingIdentifier() -> String? {
+        if let buildingNumber = buildingNumber, let street = street {
+            return "\(buildingNumber) \(street)"
+        } else if buildingName != nil {
+            return buildingName
+        } else {
+            return nil
+        }
+    }
+}
