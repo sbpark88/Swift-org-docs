436e6c84af5a8e409e2246d77ede430bf7a3da45
sbpark88 <saebyulpark88@gmail.com>
2022-12-23 12:58:42 +0900

    Update: Complete the section 2.3 - Converting Errors to Optional Values
    Ref: https://docs.swift.org/swift-book/LanguageGuide/ErrorHandling.html

--- Chapter 16 - Error Handling.playground/Pages/2.3. Converting Errors to Optional Values.xcplaygroundpage/Contents.swift
+++ Chapter 16 - Error Handling.playground/Pages/2.3. Converting Errors to Optional Values.xcplaygroundpage/Contents.swift
@@ -1,1 +1,51 @@
 import Foundation
+
+enum SomeError: Error {
+case zero
+}
+
+func someOptionalFunction(_ input: Int) -> Int? {
+    if input == 0 {
+        return nil
+    } else {
+        return input
+    }
+}
+
+let x: Int? = someOptionalFunction(0)
+print(x as Any)                         // nil
+let y: Int? = someOptionalFunction(1)
+print(y as Any)                         // Optional(1)
+
+
+print("---------------------------------------------")
+func someThrowingFunction(_ input: Int) throws -> Int {
+    if input == 0 {
+        throw SomeError.zero
+    } else {
+        return input
+    }
+}
+
+let p = try? someThrowingFunction(0)
+print(p as Any)                         // nil
+let q = try? someThrowingFunction(1)
+print(q as Any)                         // Optional(1)
+
+
+print("---------------------------------------------")
+let v: Int?
+do {
+    v = try someThrowingFunction(0)
+} catch is SomeError {
+    v = nil
+}
+print(v as Any)                         // nil
+
+let w: Int?
+do {
+    w = try someThrowingFunction(1)
+} catch is SomeError {
+    w = nil
+}
+print(w as Any)                         // Optional(1)
