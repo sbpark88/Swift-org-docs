1001fbc4122fcbaef78c65f2ade21e4fd13f8ac1
sbpark88 <saebyulpark88@gmail.com>
2022-12-25 00:43:48 +0900

    Update: Complete the section 2.4 - Disabling Error Propagation
    Ref: https://docs.swift.org/swift-book/LanguageGuide/ErrorHandling.html

--- Chapter 16 - Error Handling.playground/Pages/2.4. Disabling Error Propagation.xcplaygroundpage/Contents.swift
+++ Chapter 16 - Error Handling.playground/Pages/2.4. Disabling Error Propagation.xcplaygroundpage/Contents.swift
@@ -1,1 +1,30 @@
 import Foundation
+
+enum SomeError: Error {
+case zero
+}
+
+func someThrowingFunction(_ input: Int) throws -> Int {
+    if input == 0 {
+        throw SomeError.zero
+    } else {
+        return input
+    }
+}
+
+let q = try someThrowingFunction(1)
+print(q as Any)                         // 1
+
+print("----------------------------------------")
+func someThrowingFunctionWithOptional(_ input: Int) throws -> Int? {
+    if input == 0 {
+        throw SomeError.zero
+    } else {
+        return input
+    }
+}
+
+let x = try someThrowingFunction(1)
+print(x as Any)                         // 1
+let y = try! someThrowingFunction(1)
+print(y as Any)                         // 1
