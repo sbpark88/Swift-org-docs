f55e398239073a40b9f445bf1e4991ff5d4c297d
sbpark88 <saebyulpark88@gmail.com>
2023-02-20 15:41:33 +0900

    Update: Complete the section 2 - Method Requirements
    Ref: https://docs.swift.org/swift-book/documentation/the-swift-programming-language/protocols

---
+++ Chapter 21 - Protocols.playground/Pages/2. Method Requiremenets.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    
+    func random() -> Double {
+        lastRandom = ((lastRandom + a + c).truncatingRemainder(dividingBy: m))
+        return lastRandom / m
+    }
+}
+
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+print("And another one: \(generator.random())")
