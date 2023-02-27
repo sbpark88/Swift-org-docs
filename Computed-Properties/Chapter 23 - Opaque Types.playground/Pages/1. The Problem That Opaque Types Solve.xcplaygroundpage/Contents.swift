9340412a169c20e7d69df81b8b4114f33b25be45
sbpark88 <saebyulpark88@gmail.com>
2023-02-27 14:22:34 +0900

    Update: Complete the section 2 - The Problem That Opaque Types Solve
    - Nonopaque Types
    
    Ref: https://docs.swift.org/swift-book/documentation/the-swift-programming-language/opaquetypes

--- Computed-Properties/Chapter 23 - Opaque Types.playground/Pages/1. The Problem That Opaque Types Solve.xcplaygroundpage/Contents.swift
+++ Computed-Properties/Chapter 23 - Opaque Types.playground/Pages/1. The Problem That Opaque Types Solve.xcplaygroundpage/Contents.swift
@@ -1,1 +1,55 @@
 import Foundation
+
+protocol Shape {
+    func draw() -> String
+}
+
+
+// 1. Triangle
+struct Triangle: Shape {
+    var size: Int
+    func draw() -> String {
+        var result: [String] = []
+        for length in 1...size {
+            result.append(String(repeating: "*", count: length))
+        }
+        return result.joined(separator: "\n")
+    }
+}
+
+let smallTriangle = Triangle(size: 3)
+print(smallTriangle)    // Triangle(size: 3)
+
+//var arr = ["*", "**", "***"]
+//print(arr)                          // ["*", "**", "***"]
+//print(arr.joined())                 // ******
+//print(arr.joined(separator: ", "))  // *, **, ***
+
+print(smallTriangle.draw())
+
+
+// 2. FlippedShape
+struct FlippedShape<T: Shape>: Shape {
+    var shape: T
+    func draw() -> String {
+        let lines = shape.draw().split(separator: "\n")
+        return lines.reversed().joined(separator: "\n")
+    }
+}
+
+let flippedTriangle = FlippedShape(shape: smallTriangle)
+print(flippedTriangle.draw())
+
+
+// 3. JoinedShape
+
+struct JoinedShape<T: Shape, U: Shape>: Shape {
+    var top: T
+    var bottom: U
+    func draw() -> String {
+        top.draw() + "\n" + bottom.draw()
+    }
+}
+
+let joinedTriangles = JoinedShape(top: smallTriangle, bottom: flippedTriangle)
+print(joinedTriangles.draw())
