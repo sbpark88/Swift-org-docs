dce7f5da000da355bacc258bcbcfe167c16adb2a
sbpark88 <saebyulpark88@gmail.com>
2023-03-13 22:42:08 +0900

    Update: Complete the section 1 - Memory Safety
    Ref: https://docs.swift.org/swift-book/documentation/the-swift-programming-language/memorysafety

--- Chapter 25 - Memory Safety.playground/Pages/1.2. Understanding Conflicting Access to Memory.xcplaygroundpage/Contents.swift
+++ Chapter 25 - Memory Safety.playground/Pages/1.2. Understanding Conflicting Access to Memory.xcplaygroundpage/Contents.swift
@@ -1,3 +1,16 @@
 import Foundation
 
+// A write access to the memory where one is stored.
+var one = 1
 
+// A read access from the memory where one is stored.
+print("We're number \(one)!")
+
+
+func oneMore(than number: Int) -> Int {
+    return number + 1
+}
+
+var myNumber = 1
+myNumber = oneMore(than: myNumber)
+print(myNumber) // 2
