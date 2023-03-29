a69827b78598f38e1bdb49d3d027a42f92e8935d
sbpark88 <saebyulpark88@gmail.com>
2023-03-30 06:11:35 +0900

    Update: Complete the section 1 & 2 - Custom Types
    Ref: https://docs.swift.org/swift-book/documentation/the-swift-programming-language/accesscontrol

--- Chapter 26 - Access Control.playground/Pages/1. Access Control Syntax.xcplaygroundpage/Contents.swift
+++ Chapter 26 - Access Control.playground/Pages/1. Access Control Syntax.xcplaygroundpage/Contents.swift
@@ -1,1 +1,14 @@
 import Foundation
+
+public class SomePublicClass {}
+internal class SomeInternalClass {}
+fileprivate class SomeFilePrivateClass {}
+private class SomePrivateClass {}
+
+public var somePublicVariable = 0
+internal let someInternalConstant = 0
+fileprivate func someFilePrivateFunction() {}
+private func somePrivateFunction() {}
+
+class AnotherSomeInternalClass {}              // implicitly internal
+let anotherSomeInternalConstant = 0            // implicitly internal
